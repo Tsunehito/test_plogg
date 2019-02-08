567720c457979421d0ce31288f0376952ae38c45
Tsunehito Higa <tsunehiga0310@gmail.com>
2019-02-08 11:30:39 +0100

    added a method f_rand()

--- Date.php
+++ Date.php
@@ -4,7 +4,7 @@ class Date
 {
   /**
    * To check a day of week.
-   * @param string date
+   * @param date $date
    * @return int 0 => sunday, 1 => monday, ... 6 => Saturday
    */
   static function checkDayOfWeek($date)
@@ -15,9 +15,9 @@ class Date
   }
   /**
    * method to get a number of weekdays
-   * @param  string $start_date
-   * @param  string $end_date
-   * @return int $numWeekdays
+   * @param  date $start_date
+   * @param  date $end_date
+   * @return integer $numWeekdays
    */
   static function getNumWeekdays($start_date, $end_date)
   {
@@ -37,8 +37,36 @@ class Date
     return $numWeekdays;
   }
 
-  // static function getBaseline()
+  /**
+   * calculation of minimum value
+   * @param  int $baseline  0 ~ 100
+   * @param  int $numWeekdays
+   * @return float
+   */
+  static function calculBaseline($baseline, $numWeekdays)
+  {
+    try {
+      if ($baseline >= 0 && $baseline <= 100){
+        $min = $baseline / $numWeekdays;
+        return (float)$min;
+      }
+      else
+        throw new Exception('Please enter an integer from 0 to 100 !');
+    } catch(Exception $e){
+      echo $e->getMessage();
+    }
+  }
+
+  static function f_rand($min, $max)
+  {
+    $randomFloat = $min + mt_rand() / mt_getrandmax() * ($max - $min);
+    $randomFloat = round($randomFloat, 2);
+    return $randomFloat;
+  }
 }
 
 echo Date::checkDayOfWeek('2019-02-02') . "\n";
-echo Date::getNumWeekdays('2019-02-02', '2019-02-07');
+echo Date::getNumWeekdays('2019-02-02', '2019-02-07') . "\n";
+$weekdays = Date::getNumWeekdays('2019-02-02', '2019-02-08');
+echo Date::calculBaseline(500, $weekdays) . "\n";
+echo Date::f_rand(0,100);
